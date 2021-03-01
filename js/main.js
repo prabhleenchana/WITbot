2c34a506d4adb1e4b30158879f19c44285f7748a
prabhleenchana <56440660+prabhleenchana@users.noreply.github.com>
2021-03-01 20:54:11 +0000

    user option reply

--- js/main.js
+++ js/main.js
@@ -1,59 +1,30 @@
-
 //elements - declaring variables
-var sendBtn = document.getElementById('sendBtn');
 var textbox = document.getElementById('textbox');
-// reference to container
+var sendBtn = document.getElementById('sendBtn');
+// references to container
 var chatContainer = document.getElementById('chatContainer');
 var userName = ""; // user name input
+
+//regular expression for letters ONLY
+const regex = /^[a-zA-Z]+$/;
 
 var user = { message: "", counter: 0 };
 // ^^ global variable - stores users latest message
 
-//coding integrated and learnt from Udemy Javascript course
-
-var questionsToAsk = [
-    // { "question": "Hi there ! I'm WITbot", "answer": "" },
-    // { "question": "What's your name?", "answer": "" },
-    // { "question": "What area would you like more insight into?", "answer": "" },
-
-];
+//parts of coding integrated and learnt from Udemy Javascript course
+chatbotSendMessage("Hey there, what's your name?");
 
 let options = [
     {},
-    { number: 1, choice: "1", buttonClicked: "<button style= 'margin:5px; color:white; background:gray' onClick=botResponseToUser('1')>Careers in CS</button>" },
-    { number: 2, choice: "2", buttonClicked: "<button style= 'margin:5px; color:white; background:gray' onClick=botResponseToUser('2')>Opportunities</button>" },
-    { number: 3, choice: "3", buttonClicked: "<button style= 'margin:5px; color:white; background:gray' onClick=botResponseToUser('3')>Resources</button>" },
-    { number: 4, choice: "4", buttonClicked: "<button style= 'margin:5px; color:white; background:gray' onClick=botResponseToUser('4')>Skills in CS</button>" },
-    { number: 5, choice: "5", buttonClicked: "<button style= 'margin:5px; color:white; background:gray' onClick=botResponseToUser('5')>Women in Tech</button>" },
+    { number: 1, choice: "1", buttonClicked: "<button style= 'margin:5px; color:white; background:gray' onClick=userOptionSelection('1')>Careers in CS</button>" },
+    { number: 2, choice: "2", buttonClicked: "<button style= 'margin:5px; color:white; background:gray' onClick=userOptionSelection('2')>Opportunities</button>" },
+    { number: 3, choice: "3", buttonClicked: "<button style= 'margin:5px; color:white; background:gray' onClick=userOptionSelection('3')>Resources</button>" },
+    { number: 4, choice: "4", buttonClicked: "<button style= 'margin:5px; color:white; background:gray' onClick=userOptionSelection('4')>Skills in CS</button>" },
+    { number: 5, choice: "5", buttonClicked: "<button style= 'margin:5px; color:white; background:gray' onClick=userOptionSelection('5')>Women in Tech</button>" },
 
 ];
 
-
-chatbotSendMessage("Hey there, what's your name?");
-//askQuestion();
-// chatbotSendMessage("Hi, I'm WITbot! Here to help ");
-// chatbotSendMessage("Please pick an option: ");
-//showMenu(); // chatbot 4
-
-// chat asks questions
-function askQuestion() {
-
-
-    if (questionsToAsk.length > user.counter) {
-        setTimeout(function () {
-
-            chatbotSendMessage(questionsToAsk[user.counter].question);
-            user.counter++;
-        }, 1000);
-
-
-        console.log(questionsToAsk[user.counter - 1]);
-    }
-
-}
-
 function chatbotSendMessage(messageText) {
-
 
     var messageElement = document.createElement('div');
     messageElement.classList.add('w-50');
@@ -65,15 +36,12 @@ function chatbotSendMessage(messageText) {
     messageElement.innerHTML = "<span>Chatbot: </span>" +
         "<span style=" + "margin-top:10px; padding:10px" + ">" + messageText + "</span>";
 
-    messageElement.animate([{ easing: "ease-in", opacity: 0.4 }, { opacity: 1 }], { duration: 1000 }); // message animations
+    messageElement.animate([{ easing: "ease-in", opacity: 0.3 }, { opacity: 1 }], { duration: 1500 }); // message animations
     // parse text 
-
-
     chatContainer.appendChild(messageElement);
 
     //scroll to last message
     chatContainer.scrollTop = chatContainer.scrollHeight;
-
 }
 
 function sendMessage(messageText) {
@@ -89,7 +57,7 @@ function sendMessage(messageText) {
     messageElement.innerHTML = "<span>You: </span>" +
         "<span style=" + "margin-top:10px; padding:10px" + ">" + messageText + "</span>";
 
-    messageElement.animate([{ easing: "ease-in", opacity: 0.4 }, { opacity: 1 }], { duration: 1000 }); // message animations
+    messageElement.animate([{ easing: "ease-in", opacity: 0.3 }, { opacity: 1 }], { duration: 1500 }); // message animations
 
     // parse text 
     chatContainer.appendChild(messageElement);
@@ -97,59 +65,41 @@ function sendMessage(messageText) {
     //scroll to last message
     chatContainer.scrollTop = chatContainer.scrollHeight;
 
-
 }
 
-function botResponseToUser(messageText) {
+function userOptionSelection(messageText) {
     let userChoice = parseInt(messageText.trim()); // store in var/convert string to int/and trim any spaces from user choice
-console.log(messageText);
-console.log(userChoice);
+    // console.log(messageText);
+    // console.log(userChoice);
 
     switch (userChoice) {
 
         case 1:
-            chatbotSendMessage('you chose Careers in CS');
-            chatbotSendMessage('Something else? if yes choose a new number or 50 to end');
-
+            sendMessage('Careers in CS');
             break; // brk each time to get out of switch 
         case 2:
-            chatbotSendMessage('you chose Opportunities');
-            chatbotSendMessage('Something else? if yes choose meal number or 50 to checkout');
+            sendMessage('Opportunities');
             break;
         case 3:
-            chatbotSendMessage('you chose Resources');
-            chatbotSendMessage('Something else? if yes choose meal number or 50 to checkout');
+            sendMessage('Resources');
             break;
 
         case 4:
-            chatbotSendMessage('you chose Skills in CS');
-            chatbotSendMessage('Something else? if yes choose meal number or 50 to checkout');
+            sendMessage('Skills in Computer Science');
             break;
 
         case 5:
-            chatbotSendMessage('you chose Women in Tech');
-            user.meals.push(options[5]);
-            chatbotSendMessage('Something else? if yes choose meal number or 50 to checkout');
+            sendMessage('Women in Tech');
             break;
-        case 50:
-            alert('checkout');
-            // chatbotSendMessage("You ordered: meals number "+ user.meals);
-            chatbotSendMessage("Total Price : $" + getTotalPrice());
-            chatbotSendMessage("please click this link to complete checkout:");
-            chatbotSendMessage("<a href='https://google.com'>Checkout</a>");
-        //send meals + ticket + payment status to backend
-
-        //     break;
-        // default:               // if user choices matches nothing 
-        //     alert("Please choose a valid number");  // 1/2/3 choice option error
-        //     chatbotSendMessage("Please choose a valid number");// option error in chat
+        default:               // if user choices matches nothing 
+            alert("Please choose a valid option");  // 1/2/3 choice option error
+            chatbotSendMessage("Please choose a valid number");// option error in chat
     }
 
     // console.log(user);
-
 }
 
-function showMenu() {
+function optionsSelection() {
 
     var messageElement = document.createElement('div');
     messageElement.classList.add('w-50');
@@ -165,7 +115,7 @@ function showMenu() {
     }
 
     messageElement.animate([{ easing: "ease-in", opacity: 0.4 }, { opacity: 1 }], { duration: 2000 });
-    chatContainer.appendChild(messageElement);     // parse text 
+    chatContainer.appendChild(messageElement);     // parse text - string to int
 
 }
 
@@ -177,12 +127,12 @@ sendBtn.addEventListener('click', function (e) {
 
         if (isNaN(userName)) { // if username not a number - execute this
 
-
             sendMessage(messageText);
             textbox.value = "";
-            chatbotSendMessage("Hi " + userName + ", I'm WITbot");
-            showMenu();
-     //       askQuestion(messageText);
+            chatbotSendMessage("Hi " + userName + ", I'm WITbot !");
+            chatbotSendMessage("Here to help, pick an option:");
+            optionsSelection();
+
         }
 
         else {
@@ -196,17 +146,15 @@ sendBtn.addEventListener('click', function (e) {
 
     } else {
 
-
         let messageText = textbox.value.trim();
         user.message = messageText;
         sendMessage(messageText);
         textbox.value = "";
 
-        botResponseToUser(messageText);
+        userOptionSelection(messageText);
 
     }
 });
-
 
 textbox.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
@@ -219,12 +167,11 @@ textbox.addEventListener('keypress', function (e) {
 
             if (isNaN(userName)) { // if username not a number - execute this
 
-
                 sendMessage(messageText);
                 textbox.value = "";
                 chatbotSendMessage("Hi " + userName + ", I'm WITbot");
-                showMenu();
-               // askQuestion(messageText);
+                chatbotSendMessage("Here to help, pick an option:");
+                optionsSelection();
             }
 
             else {
@@ -237,13 +184,12 @@ textbox.addEventListener('keypress', function (e) {
 
         } else {
 
-
             let messageText = textbox.value.trim(); // trim for spaces
             user.message = messageText; // for processing user message 
             sendMessage(messageText);
             textbox.value = ""; //message box to be empty after sending text
 
-            botResponseToUser(messageText);
+            userOptionSelection(messageText);
 
         }
 
